65359bcd8bb65d67b3de12e4a00cd877b781d1c2
imwalterbot <127900942+imwalterbot@users.noreply.github.com>
2023-03-18 13:01:04 -0700

    Generated code by the GitHub bot

--- index.ts
+++ index.ts
@@ -1,119 +1,3 @@
-import { Webhooks, createNodeMiddleware } from "@octokit/webhooks";
-import express from "express";
-import {
-  generatePseudocodeFromEmbedded,
-  postComment,
-  createEdit,
-  createEditWithChat,
-} from "./psudocode-generator";
-import { handleCodeGeneration, createNewPullRequest } from "./code-committer";
-import Config from "./config";
-import octokit from "./gh";
-import * as utils from "./utils";
-import { parseCommentForJobs } from "./job-interpreter";
-import winston from "./log";
-
-const BOT_NAME = Config.githubBotName;
-const BOT_LABEL = "walter-build";
-
-const webhooks = new Webhooks({ secret: Config.githubWebhookSecret });
-
-function isBotTask(issue: any, repository: string, user: string): boolean {
-  if (
-    !Config.supportedRepos.includes(repository) &&
-    !Config.supportedUsers.includes(user)
-  ) {
-    return false;
-  }
-  return issue.labels.some((label: any) => label.name === BOT_LABEL);
-}
-
-export async function extractTaskInfoAndEmbed(
-  issueBody: string
-): Promise<string> {
-  const githubUrlRegex =
-    /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
-
-  let description = issueBody;
-
-  const matches = githubUrlRegex.exec(issueBody);
-
-  const files: { link: string; snippit: string }[] = [];
-
-  if (matches == null) {
-    return description;
-  }
-
-  for (let i = 0; i < matches.length; i++) {
-    const match = matches[i];
-    const parsedUrl = utils.parseGitHubURL(match);
-    winston.log("info", parsedUrl);
-
-    if (!parsedUrl) continue;
-
-    try {
-      const fileContentResponse = await octokit.rest.repos.getContent({
-        owner: parsedUrl.owner,
-        repo: parsedUrl.repo,
-        path: parsedUrl.filePath,
-        ref: parsedUrl.branch,
-      });
-
-      const fileContent = Buffer.from(
-        (fileContentResponse.data as any).content,
-        "base64"
-      ).toString();
-
-      winston.log("info", "file contents", fileContent);
-
-      files.push({
-        link: match,
-        snippit: fileContent,
-      });
-    } catch (err: any) {
-      winston.log(
-        "error",
-        `Error fetching file content from GitHub: ${err.message}`
-      );
-    }
-  }
-
-  return description.trim();
-}
-
-function extractTaskInfo(issue: any): {
-  description: string;
-  files: string[];
-  lines: number[];
-} {
-  const description = issue.body;
-  const fileRegex = /file:\s*([\w./-]+)/gi;
-  const lineRegex = /line:\s*(\d+)/gi;
-
-  const files: string[] = [];
-  const lines: number[] = [];
-
-  let fileMatch = fileRegex.exec(description);
-  while (fileMatch) {
-    files.push(fileMatch[1]);
-    fileMatch = fileRegex.exec(description);
-  }
-
-  let lineMatch = lineRegex.exec(description);
-  while (lineMatch) {
-    lines.push(parseInt(lineMatch[1], 10));
-    lineMatch = lineRegex.exec(description);
-  }
-
-  return { description, files, lines };
-}
-
-type CommentAction =
-  | { type: "unknown" }
-  | { type: "refine"; body: string; files?: string[]; lines?: number[] }
-  | { type: "approve" }
-  | { type: "design"; body: string };
-
 webhooks.on("issue_comment.created", async (event: any) => {
   const comment = event.payload.comment;
   const issue = event.payload.issue;
@@ -129,6 +13,7 @@ webhooks.on("issue_comment.created", async (event: any) => {
 
     try {
       if (action.type === "refine") {
+        winston.log("info", "Action type: Refine");
         const hist: any = [];
         const jobs = parseCommentForJobs(action.body);
         winston.log("info", "jobs", jobs);
@@ -139,6 +24,7 @@ webhooks.on("issue_comment.created", async (event: any) => {
         }
         return;
       } else if (action.type === "approve") {
+        winston.log("info", "Action type: Approve");
         winston.log("info", "approved");
         const hist = await utils.getCommentHistory(repository, issue.number);
         const previousDevMsgs = hist.filter((v) => {
@@ -158,6 +44,7 @@ webhooks.on("issue_comment.created", async (event: any) => {
         );
         winston.log("info", "processing complete");
       } else if (action.type === "design") {
+        winston.log("info", "Action type: Design");
         processDesignAction(action.body);
       }
     } catch (err) {
@@ -167,41 +54,3 @@ webhooks.on("issue_comment.created", async (event: any) => {
     winston.log("info", "Ignoring comment");
   }
 });
-
-function parseComment(comment: any): CommentAction {
-  const refineRegex = /refine\s*:\s*(.+)/i;
-  const approveRegex = new RegExp(`@${BOT_NAME}\\s*APPROVED`, "i");
-  const designRegex = /design\s*:\s*(.+)/i;
-
-  if (approveRegex.test(comment.body)) {
-    return { type: "approve" };
-  }
-
-  if (designRegex.test(comment.body)) {
-    return { type: "design", body: comment.body };
-  }
-
-  return { type: "refine", body: comment.body };
-}
-
-async function parseCommentWithEmbedded(comment: any): Promise<CommentAction> {
-  const myParsed = parseComment(comment);
-  if (myParsed.type != "refine") {
-    return myParsed;
-  }
-
-  const taskInfo = await extractTaskInfoAndEmbed(myParsed.body as string);
-  return { type: "refine", body: taskInfo };
-}
-
-function processDesignAction(commentBody: string) {
-  winston.log("info", `Design action comment body: ${commentBody}`);
-}
-
-const middleware = createNodeMiddleware(webhooks, { path: "/webhook" });
-
-const app = express();
-app.use(middleware);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => winston.log("info", `Server listening on port ${PORT}`));
