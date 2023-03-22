fac82f08102a0b00a31f4b1be60bb6b3b46b070a
James Smoot <jimi@jsfour.com>
2023-03-22 10:03:51 -0700

    Make env params.

--- src/config.ts
+++ src/config.ts
@@ -6,13 +6,9 @@ export default {
   githubApiToken: process.env.GITHUB_API_TOKEN || "",
   githubWebhookSecret: process.env.GITHUB_WEBHOOK_SECRET || "",
   openApiKey: process.env.OPENAI_API_KEY || "",
-  githubBotName: "imwalterbot",
-  supportedRepos: [
-    "octaviuslabs/walter",
-    "octaviuslabs/mailmentor-api",
-    "octaviuslabs/mailmentor-ui",
-  ],
-  supportedUsers: ["jsfour"],
+  githubBotName: process.env.GITHUB_USERNAME,
+  supportedRepos: (process.env.SUPPORTED_REPOS || "").split(","), // "octaviuslabs/walter,foo/bar"
+  supportedUsers: (process.env.SUPPORTED_USERS || "").split(","), // "jsfour,AdaLovelace"
   saveInteractions: process.env.SAVE_INTERACTIONS == "true",
   botTaskLabel: "walter-build",
 };
